9fa06d09740971105baa3a39c094e7c862e32845
nasre21 <nasou2001@gmail.com>
2023-10-19 14:53:33 +0200

    its done

--- src/app/components/employee-details/employee-details.component.ts
+++ src/app/components/employee-details/employee-details.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Employee } from 'src/app/model/employee';
+import { EmployeeService } from 'src/app/services/employee.service';
 
 @Component({
   selector: 'app-employee-details',
   templateUrl: './employee-details.component.html',
   styleUrls: ['./employee-details.component.scss']
 })
-export class EmployeeDetailsComponent {
+export class EmployeeDetailsComponent implements OnInit {
+
+  id!: number;
+  employee!:Employee
+
+  constructor(private employeeService: EmployeeService, private route: ActivatedRoute){}
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id']
+
+    this.employee = new Employee()
+    this.employeeService.getEmployeeId(this.id).subscribe({
+      next:(data) =>{
+        this.employee = data;
+      },
+      error:(err) =>console.log(err)
+    })
+  }
+
+
 
 }
